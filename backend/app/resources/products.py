57c7931cfbfd4ccc7d7504888dd79bd0d66d10f1
Fotis Xenakis <foxen@windowslive.com>
2019-01-11 20:48:54 +0200

    Complete + Tested implementation of products resource

--- backend/app/resources/products.py
+++ backend/app/resources/products.py
@@ -1,17 +1,34 @@
 from flask_restful import Resource
 from webargs import fields
 from webargs.flaskparser import use_args
-from marshmallow import validate
+import marshmallow
+from marshmallow import validate, post_dump
 
-from app.models import db, ma, Product
+from app.models import db, ma, Product, ProductTag
 from app.resources.auth import requires_auth
 
+class ProductTagSchema(ma.ModelSchema):
+    # This is necessary because we require tags to be a list of names (strings),
+    # but by default it would be a list of {'name': <name>} dicts
+    @post_dump
+    def flatten(self, data):
+        return data['name']
+
+    class Meta:
+        model = ProductTag
+        # Fields to be included in the output
+        fields = ('name', )
+
 class ProductSchema(ma.ModelSchema):
-    # TODO: set which fields are to be serialized
+    # Override tags field to use a nested representation rather than keys
+    tags = marshmallow.fields.Nested(ProductTagSchema, many=True)
+
     class Meta:
         model = Product
+        # Fields to be included in the output
+        fields = ('id', 'name', 'description', 'category', 'tags', 'withdrawn')
 
-bad_request = '', 400
+
 prod_schema = ProductSchema()
 
 
@@ -38,27 +55,31 @@ class ProductsResource(Resource):
             'name|ASC': Product.name.asc(),
             'name|DESC': Product.name.desc()
         }[args['sort']]
-        products = Product.query.filter_by(withdrawn=status).order_by(sort). \
-                paginate(start, count, False)  # don't return 404 if there are no products
+        query = Product.query
+        if status != 'ALL':
+            query = query.filter_by(withdrawn=(status == 'WITHDRAWN'))
+        products = query.order_by(sort).paginate(start, count, False)
 
         return {
             'start': products.page - 1,
-            'count': products.items,
+            'count': len(products.items),
             'total': products.total,
             'products': prod_schema.dump(products.items, many=True).data
         }
 
     @requires_auth
     @use_args({
-        'name': fields.Str(location='json', required=True),
-        'description': fields.Str(location='json', required=True),
-        'category': fields.Str(location='json', required=True),
-        'tags': fields.List(fields.Str(), location='json', required=True),
+        'name': fields.Str(location='form', required=True),
+        'description': fields.Str(location='form', required=True),
+        'category': fields.Str(location='form', required=True),
+        'tags': fields.List(fields.Str(), location='form', required=True),
         'format': fields.Str(missing='json', location='query', validate=validate.Equal('json'))
     })
     def post(self, args, **_kwargs):
-        del args['format']  # don't pass format to Product's constructor
-        product = Product(**{**args, 'withdrawn': False})
+        product = Product(name=args['name'], description=args['description'],
+                          category=args['category'], withdrawn=False)
+        product.tags = [ProductTag(name=tag, product=product) for tag in
+                        args['tags'] if tag.strip()] # ignore blank tags
         db.session.add(product)
         db.session.commit()
 
@@ -68,45 +89,54 @@ class ProductResource(Resource):
     @use_args({
         'format': fields.Str(missing='json', location='query', validate=validate.Equal('json'))
     })
-    def get(self, prod_id, **_kwargs):
+    def get(self, _args, prod_id):
         product = Product.query.get_or_404(prod_id)
 
         return prod_schema.dump(product).data
 
     @requires_auth
     @use_args({
-        'name': fields.Str(location='json', required=True),
-        'description': fields.Str(location='json', required=True),
-        'category': fields.Str(location='json', required=True),
-        'tags': fields.List(fields.Str(), location='json', required=True),
+        'name': fields.Str(location='form', required=True),
+        'description': fields.Str(location='form', required=True),
+        'category': fields.Str(location='form', required=True),
+        'tags': fields.List(fields.Str(), location='form', required=True),
         'format': fields.Str(missing='json', location='query', validate=validate.Equal('json'))
     })
-    def put(self, prod_id, args, **_kwargs):
+    def put(self, args, prod_id, **_kwargs):
         product = Product.query.get_or_404(prod_id)
         product.name = args['name']
         product.description = args['description']
         product.category = args['category']
-        product.tags = args['tags']
+        for tag in product.tags:
+            db.session.delete(tag)  # delete old product tags
+        product.tags = [ProductTag(name=tag, product=product) for tag in
+                        args['tags'] if tag.strip()]   # ignore blank tags
+
         db.session.commit()
 
         return prod_schema.dump(product).data
 
     @requires_auth
     @use_args({
-        'name': fields.Str(location='json'),
-        'description': fields.Str(location='json'),
-        'category': fields.Str(location='json'),
-        'tags': fields.List(fields.Str(), location='json'),
+        'name': fields.Str(location='form'),
+        'description': fields.Str(location='form'),
+        'category': fields.Str(location='form'),
+        'tags': fields.List(fields.Str(), location='form'),
         'format': fields.Str(missing='json', location='query', validate=validate.Equal('json'))
     })
-    def patch(self, prod_id, args, **_kwargs):
-        if len(args) != 2:
-            # Only one of name, description, category and tags should be specified
-            return bad_request
+    def patch(self, args, prod_id, **_kwargs):
         del args['format']
-        changed = next(iter(args.keys()))
+        if len(args) != 1:
+            return 'Specify exactly one of: name, description, category or tags', 400
         product = Product.query.get_or_404(prod_id)
-        setattr(product, changed, args[changed])
+        changed = next(iter(args.keys()))
+        if changed == 'tags':
+            for tag in product.tags:
+                db.session.delete(tag)  # delete old product tags
+            product.tags = [ProductTag(name=tag, product=product) for tag in
+                            args['tags'] if tag.strip()]    # ignore blank tags
+        else:
+            setattr(product, changed, args[changed])
         db.session.commit()
 
         return prod_schema.dump(product).data
@@ -115,7 +145,7 @@ class ProductResource(Resource):
     @use_args({
         'format': fields.Str(missing='json', location='query', validate=validate.Equal('json'))
     })
-    def delete(self, prod_id, is_admin, **_kwargs):
+    def delete(self, _args, prod_id, is_admin, **_kwargs):
         product = Product.query.get_or_404(prod_id)
 
         if is_admin:
@@ -124,4 +154,5 @@ class ProductResource(Resource):
             product.withdrawn = True
 
         db.session.commit()
+
         return {'message': 'OK'}
