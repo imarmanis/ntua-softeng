24082a4345d151d9c6cb266ff672db9d655a605d
Fotis Xenakis <foxen@windowslive.com>
2019-01-11 02:27:04 +0200

    Added import for ProductResource

--- backend/app/resources/__init__.py
+++ backend/app/resources/__init__.py
@@ -1,5 +1,5 @@
 from flask_restful import Api
-from app.resources.products import ProductsResource
+from app.resources.products import ProductsResource, ProductResource
 from app.resources.prices import PricesResource
 from app.resources.auth import LoginResource, LogoutResource
 from webargs.flaskparser import parser, abort
