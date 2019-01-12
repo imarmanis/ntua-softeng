23dda89b8c251bb915708129df66d61a7d2bd09c
Fotis Xenakis <foxen@windowslive.com>
2019-01-12 16:54:46 +0200

    Closes #7

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,7 +3,7 @@ from config import Config
 
 
 def create_app(config_class=Config):
-    app = Flask("restapi", instance_relative_config=True)
+    app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(config_class)
     app.config.from_pyfile('config.py')
 
