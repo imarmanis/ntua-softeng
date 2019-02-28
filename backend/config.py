b9c214a8b2c63fa73860cd28b8705ae0adc3ffd2
Iaswn Marmanis <j.ason_96@hotmail.com>
2019-02-28 14:12:51 +0200

    Change flask env to production and don't echo sql by default

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,4 @@
 class Config(object):
-    SQLALCHEMY_ECHO = True
+    SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://root:root@localhost:5432/restapi'
