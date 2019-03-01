052bbb7743554eeb64be853cd32ace9f6c07a625
Iaswn Marmanis <j.ason_96@hotmail.com>
2019-03-01 12:26:37 +0200

    Refactor tests

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,10 +1,9 @@
 import pytest
 from app import create_app
-from app.models import db, User
-from tests import data
+from app.models import db
 
 
-@pytest.fixture(scope="module", params=['postgresql+psycopg2://root:root@localhost:5432/testing'], autouse=True)
+@pytest.fixture(scope="class", params=['postgresql+psycopg2://root:root@localhost:5432/testing'], autouse=True)
 def client(request):
     app = create_app()
     app.config['SQLALCHEMY_DATABASE_URI'] = request.param
@@ -15,13 +14,3 @@ def client(request):
         yield app.test_client()
         db.session.remove()
         db.drop_all()
-
-
-@pytest.fixture(scope="function")
-def user1_token():
-    return User.query.filter(User.username == data.users[1]['username']).first().token
-
-
-@pytest.fixture(scope="module")
-def user1():
-    return data.users[1]
--- backend/tests/test_basic.py
+++ backend/tests/test_basic.py
@@ -1,6 +1,6 @@
 import pytest
 from app.models import User
-from tests import url_for, auth_header
+from tests.utils import url_for, auth_header
 from tests import data
 from collections import Counter
 
@@ -14,6 +14,15 @@ def valid_response(real_data, json):
 
 
 class TestBasic(object):
+
+    @staticmethod
+    def user1_token():
+        return User.query.filter(User.username == data.users[1]['username']).first().token
+
+    @staticmethod
+    def user1():
+        return data.users[1]
+
     @pytest.mark.parametrize("user_data", data.users)
     def test_register(self, client, user_data):
         rv = client.post(url_for('/register'))
@@ -23,34 +32,40 @@ class TestBasic(object):
         assert rv.status_code == 200
         assert User.query.filter(User.username == user_data['username']).first() is not None
         assert User.query.filter(User.username == user_data['username']).first().verify_password(user_data['password'])
-        assert User.query.filter(User.username == user_data['username']).first().is_admin == False
+        assert User.query.filter(User.username == user_data['username']).first().is_admin is False
         assert User.query.filter(User.username == user_data['username']).first().token is None
 
         rv = client.post(url_for('/register'), data=user_data)
         assert rv.status_code == 400
 
-    def test_login(self, client, user1):
+    def test_login(self, client):
         rv = client.post(url_for('/login'))
         assert rv.status_code == 400
 
-        rv = client.post(url_for('/login'), data=user1)
+        rv = client.post(url_for('/login'), data=self.user1())
         assert rv.status_code == 200
         assert rv.json['token'] == User.query.filter(User.username == data.users[1]['username']).first().token
 
     @pytest.mark.parametrize("product_data", data.products)
-    def test_add_product(self, client, product_data, user1_token):
+    def test_add_product(self, client, product_data):
         rv = client.post(
             url_for('/products'),
-            headers=auth_header(user1_token),
+            headers=auth_header(self.user1_token()),
             data=product_data
         )
         assert rv.status_code == 200
         valid_response(product_data, rv.json)
 
-    def test_list_products(self, client, user1_token):
+    def test_list_products(self, client):
         rv = client.get(
-            url_for("/products?start=0&count=10&status=ACTIVE&sort=id%7CASC"),
-            headers=auth_header(user1_token)
+            url_for('/products'),
+            query_string={
+                'start': 0,
+                'count': 10,
+                'status': 'ACTIVE',
+                'sort': 'id|ASC'
+            },
+            headers=auth_header(self.user1_token())
         )
 
         assert rv.status_code == 200
@@ -61,7 +76,7 @@ class TestBasic(object):
         for i, product in enumerate(data.products):
             valid_response(product, rv.json['products'][i])
 
-    def test_update_product(self, client, user1_token):
+    def test_update_product(self, client):
         prod1upd = data.products[0].copy()
         prod1upd.update({
             "name": 'bar-foo',
@@ -70,58 +85,64 @@ class TestBasic(object):
 
         rv = client.put(
             url_for("/products/1"),
-            headers=auth_header(user1_token),
+            headers=auth_header(self.user1_token()),
             data=prod1upd
         )
 
         assert rv.status_code == 200
         valid_response(prod1upd, rv.json)
 
-    def test_get_product(self, client, user1_token):
+    def test_get_product(self, client):
         rv = client.get(
             url_for("/products/2"),
-            headers=auth_header(user1_token)
+            headers=auth_header(self.user1_token())
         )
 
         assert rv.status_code == 200
         valid_response(data.products[1], rv.json)
 
-    def test_patch_product(self, client, user1_token):
+    def test_patch_product(self, client):
         prod2patch = data.products[1].copy()
         prod2patch.update({'tags': ['nope']})
         rv = client.patch(
             url_for("/products/2"),
-            headers=auth_header(user1_token),
+            headers=auth_header(self.user1_token()),
             data={'tags': prod2patch['tags']}
         )
 
         assert rv.status_code == 200
         valid_response(prod2patch, rv.json)
 
-    def test_delete_product(self, client, user1_token):
+    def test_delete_product(self, client):
         rv = client.delete(
             url_for("/products/2"),
-            headers=auth_header(user1_token)
+            headers=auth_header(self.user1_token())
         )
 
         assert rv.status_code == 200
         assert rv.json['message'] == "OK"
 
     @pytest.mark.parametrize("shop_data", data.shops)
-    def test_add_shop(self, client, shop_data, user1_token):
+    def test_add_shop(self, client, shop_data):
         rv = client.post(
             url_for('/shops'),
-            headers=auth_header(user1_token),
+            headers=auth_header(self.user1_token()),
             data=shop_data
         )
 
         assert rv.status_code == 200
         valid_response(shop_data, rv.json)
 
-    def test_list_shops(self, client, user1_token):
+    def test_list_shops(self, client):
         rv = client.get(
-            url_for("/shops?start=0&count=10&status=ACTIVE&sort=id%7CASC"),
-            headers=auth_header(user1_token)
+            url_for('/shops'),
+            query_string={
+                'start': 0,
+                'count': 10,
+                'status': 'ACTIVE',
+                'sort': 'id|ASC'
+            },
+            headers=auth_header(self.user1_token())
         )
 
         assert rv.status_code == 200
@@ -132,7 +153,7 @@ class TestBasic(object):
         for i, shop_data in enumerate(data.shops):
             valid_response(shop_data, rv.json['shops'][i])
 
-    def test_update_shop(self, client, user1_token):
+    def test_update_shop(self, client):
         shop1upd = data.shops[0].copy()
         shop1upd.update({
             "name": 'baz-foo',
@@ -141,52 +162,52 @@ class TestBasic(object):
 
         rv = client.put(
             url_for("/shops/1"),
-            headers=auth_header(user1_token),
+            headers=auth_header(self.user1_token()),
             data=shop1upd
         )
 
         assert rv.status_code == 200
         valid_response(shop1upd, rv.json)
 
-    def test_get_shop(self, client, user1_token):
+    def test_get_shop(self, client):
         rv = client.get(
             url_for("/shops/2"),
-            headers=auth_header(user1_token)
+            headers=auth_header(self.user1_token())
         )
 
         assert rv.status_code == 200
         valid_response(data.shops[1], rv.json)
 
-    def test_patch_shop(self, client, user1_token):
+    def test_patch_shop(self, client):
         shop2patch = data.shops[1].copy()
         shop2patch.update({'lat': 99})
         rv = client.patch(
             url_for("/shops/2"),
-            headers=auth_header(user1_token),
+            headers=auth_header(self.user1_token()),
             data={'lat': shop2patch['lat']}
         )
 
         assert rv.status_code == 200
         valid_response(shop2patch, rv.json)
 
-    def test_delete_shop(self, client, user1_token):
+    def test_delete_shop(self, client):
         rv = client.delete(
             url_for("/shops/2"),
-            headers=auth_header(user1_token)
+            headers=auth_header(self.user1_token())
         )
 
         assert rv.status_code == 200
         assert rv.json['message'] == "OK"
 
-    def test_logout(self, client, user1, user1_token):
+    def test_logout(self, client):
 
         rv = client.post(url_for('/logout'))
         assert rv.status_code == 403
 
-        rv = client.post(url_for('/logout'), headers=auth_header(user1_token))
+        rv = client.post(url_for('/logout'), headers=auth_header(self.user1_token()))
 
         assert rv.status_code == 200
-        assert not User.query.filter(User.username == user1['username']).first().token
+        assert not self.user1_token()
         assert rv.json['message'] == "OK"
 
 
