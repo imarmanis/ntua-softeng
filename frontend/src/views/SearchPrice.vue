631433f1962f9a587ba7cec45b374527d8d286fe
Iaswn Marmanis <j.ason_96@hotmail.com>
2019-03-02 15:38:41 +0200

    Add price view layout and reactive data presentation.
    Has major display issues, container need to use whole page.
    Maybe can be fixed with css?

--- frontend/src/views/SearchPrice.vue
+++ frontend/src/views/SearchPrice.vue
@@ -1,81 +1,94 @@
 <template>
     <div id="search-price">
-        <b-jumbotron lead="Αναζήτηση τιμής">
-            <b-form @submit.prevent="post">
-                <b-form-group
-                        id="productgroup"
-                        :invalid-feedback="errors.first('product_name')"
-                        label="Επίλεξε προιόν"
-                        label-cols=3   >
-                    <b-form-select v-model="productId"
-                                   v-validate="'required'"
-                                   name="product_name"
-                                   data-vv-as="*Το πεδίο"
-                                   :state = "errors.has('product_name') ? false :null" >
-                        <option   v-for="product in products"
-                                  v-bind:value="product.id"
-                                  v-bind:key="product.id" >
-                            {{ product.name }}
-                        </option>
-                    </b-form-select>
-                </b-form-group>
-                <b-form-group
-                        id="dategroupfrom"
-                        :invalid-feedback="errors.first('date_from')"
-                        label="Ημερομηνία  από:"
-                        label-cols = 3 >
-                    <b-form-input name="date_from" type="date"
-                                  v-validate="'required|date_format:YYYY-MM-DD'"
-                                  ref="_fromDate"
-                                  data-vv-as="*Η ημερομηνία"
-                                  v-model="dateFrom"
-                                  :state="errors.has('date_from') ? false :null"
-                    />
-                </b-form-group>
-                <b-form-group
-                        id="dategroupto"
-                        :invalid-feedback="errors.first('date_to')"
-                        label="Ημερομηνία εώς:"
-                        label-for="date-to"
-                        label-cols= 3
-                        description="Hint: Πιέστε το βελάκι για ημερολόγιο" >
-                    <b-form-input id="date_to" name="date_to" type="date"
-                                  v-validate="'required|date_format:YYYY-MM-DD|after:_fromDate,true'"
-                                  data-vv-as="*Η ημερομηνία"
-                                  v-model="dateTo"
-                                  :state="errors.has('date_to') ? false :null"
-                    />
-                </b-form-group>
-                <b-form-group
-                        :invalid-feedback="errors.first('dist')"
-                        id="distgrop" label="Ακτίνα από σημείο ενδιαφέροντος (Km):" label-for="dist"
-                        label-cols = 3 >
-                    <b-form-input
-                            name="dist"
-                            id="dist"
-                            type="text"
-                            v-validate="'decimal:2|min_value:0.01'"
-                            data-vv-as="*Η απόσταση"
-                            v-model="dist"
-                            :state="errors.has('dist') ? false :null"
-                    />
-                </b-form-group>
-                <b-form-group v-if="dist"
-                              id="shopgroup"
-                              :invalid-feedback="errors.first('my_loc')"
-                              :state="errors.has('my_loc') ? false :null"
-                              label="Σημείο ενδιαφέροντος"
-                              label-cols =3   >
-                    <myMap v-validate="'required'" data-vv-name="my_loc" data-vv-as="*Η Τοποθεσία"
-                           :with-rclick="true" :with-location="true" @input="rclick"></myMap>
-                </b-form-group>
-                <b-button type="submit" variant="primary">Αναζήτηση</b-button>
-            </b-form>
-        </b-jumbotron>
-        <myMap :data="shops"  @input="shopSelected"></myMap>
-        <div v-for="price in priceData" :key="price.id">
-            {{ price }}
-        </div>
+        <b-container fluid class="bv-example-row">
+            <b-row>
+                <b-col>
+                    <b-jumbotron lead="Αναζήτηση τιμής">
+                        <b-form @submit.prevent="post">
+                            <b-form-group
+                                    id="productgroup"
+                                    :invalid-feedback="errors.first('product_name')"
+                                    label="Επίλεξε προιόν"
+                                    label-cols=3   >
+                                <b-form-select v-model="productId"
+                                               v-validate="'required'"
+                                               name="product_name"
+                                               data-vv-as="*Το πεδίο"
+                                               :state = "errors.has('product_name') ? false :null" >
+                                    <option   v-for="product in products"
+                                              v-bind:value="product.id"
+                                              v-bind:key="product.id" >
+                                        {{ product.name }}
+                                    </option>
+                                </b-form-select>
+                            </b-form-group>
+                            <b-form-group
+                                    id="dategroupfrom"
+                                    :invalid-feedback="errors.first('date_from')"
+                                    label="Ημερομηνία  από:"
+                                    label-cols = 3 >
+                                <b-form-input name="date_from" type="date"
+                                              v-validate="'required|date_format:YYYY-MM-DD'"
+                                              ref="_fromDate"
+                                              data-vv-as="*Η ημερομηνία"
+                                              v-model="dateFrom"
+                                              :state="errors.has('date_from') ? false :null"
+                                />
+                            </b-form-group>
+                            <b-form-group
+                                    id="dategroupto"
+                                    :invalid-feedback="errors.first('date_to')"
+                                    label="Ημερομηνία εώς:"
+                                    label-for="date-to"
+                                    label-cols= 3
+                                    description="Hint: Πιέστε το βελάκι για ημερολόγιο" >
+                                <b-form-input id="date_to" name="date_to" type="date"
+                                              v-validate="'required|date_format:YYYY-MM-DD|after:_fromDate,true'"
+                                              data-vv-as="*Η ημερομηνία"
+                                              v-model="dateTo"
+                                              :state="errors.has('date_to') ? false :null"
+                                />
+                            </b-form-group>
+                            <b-form-group
+                                    :invalid-feedback="errors.first('dist')"
+                                    id="distgrop" label="Ακτίνα από σημείο ενδιαφέροντος (Km):" label-for="dist"
+                                    label-cols = 3 >
+                                <b-form-input
+                                        name="dist"
+                                        id="dist"
+                                        type="text"
+                                        v-validate="'decimal:2|min_value:0.01'"
+                                        data-vv-as="*Η απόσταση"
+                                        v-model="dist"
+                                        :state="errors.has('dist') ? false :null"
+                                />
+                            </b-form-group>
+                            <b-form-group v-if="dist"
+                                          id="shopgroup"
+                                          :invalid-feedback="errors.first('my_loc')"
+                                          :state="errors.has('my_loc') ? false :null"
+                                          label="Σημείο ενδιαφέροντος"
+                                          label-cols =3   >
+                                <myMap v-validate="'required'" data-vv-name="my_loc" data-vv-as="*Η Τοποθεσία"
+                                       :with-rclick="true" :with-location="true" @input="rclick"></myMap>
+                            </b-form-group>
+                            <b-button type="submit" variant="primary">Αναζήτηση</b-button>
+                        </b-form>
+                    </b-jumbotron>
+                </b-col>
+                <b-col v-if="showRes">
+                    <b-row>
+                        Βρέθηκαν {{ shops.length }} σχετικά καταστήματα :
+                    </b-row>
+                    <b-row>
+                        <myMap :data="shops"  @input="shopSelected"></myMap>
+                    </b-row>
+                    <b-row>
+                        <b-table striped hover :items="tdata" />
+                    </b-row>
+                </b-col>
+            </b-row>
+        </b-container>
     </div>
 </template>
 
@@ -94,13 +107,32 @@ export default {
             productId : null,
             products : [],
             shops: [],
-            priceData : [],
-            rclickedPos : null
+            selectedShop: null,
+            rclickedPos : null,
+            showRes : false
         }
+    },
+    computed: {
+        tdata: function () {
+            let res = [];
+            this.shops.forEach((sp) => {
+                sp.prices.forEach((pr) => {
+                    res.push({
+                        'price': pr.price,
+                        'date' : pr.date,
+                        'shopName': pr.shopName,
+                        '_rowVariant': this.selectedShop == pr.shopId ? 'success' : null
+                    })
+                })
+            })
+
+            return res;
+        }
+
     },
     methods: {
         shopSelected : function (shop) {
-            this.priceData = shop.prices;
+            this.selectedShop = shop.prices[0].shopId;
         },
         rclick : function (x) {
             this.rclickedPos = x
@@ -126,6 +158,7 @@ export default {
                         const prices = resp.data.prices;
                         const shopIds = Array.from(new Set(prices.map((x) => x.shopId)));
                         this.shops = []; // clear possbile previous data
+                        this.showRes = true;
                         shopIds.forEach((v) => {
                                 this.$axios.get('/shops/'+v).then((resp) => {
                                     this.shops.push({
